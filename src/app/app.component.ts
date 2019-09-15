589f604ec81bc89925b243a68266cad89947fb25
Rashikananda <rashikanandapattanaik@gmail.com>
2019-09-15 19:28:54 +0530

    last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Observable, merge, throwError, timer, interval, from, of, forkJoin,concat } from 'rxjs';
-import { switchMap, tap, takeUntil, delay, mergeMap, concatMap, map, concatAll, combineAll, toArray } from 'rxjs/operators';
+import { switchMap, tap, takeUntil, delay, mergeMap, concatMap, map, concatAll, combineAll, toArray,scan } from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -9,17 +9,22 @@ import { switchMap, tap, takeUntil, delay, mergeMap, concatMap, map, concatAll, 
 export class AppComponent {
   name = 'Angular';
   contacts;
-  rootSource$ = of([{ x: 60, y: 4 }, { x: 2, y: 4 }, { x: 1, y: 1 }]);
+  rootSource$ = of([{ x: 100, y: 4 }, { x: 20, y: 4 }, { x: 100, y: 1 }]);
   ros$;
   p =  (x) => {
     const oneSecondSource = of(x['x']).pipe(delay(100 * x.x))
     const twoSecondSource = of(x.y).pipe(delay(200 * x.y))
-    return forkJoin(oneSecondSource, twoSecondSource).pipe(map(abs => abs[0] / abs[1]));
-    return of(x)
+    return forkJoin(oneSecondSource, twoSecondSource).pipe(map(abs => {const res =abs[0] / abs[1];
+    return { ...x, z: res}
+    }));
   }
   constructor() {
-    const example1 = this.rootSource$.pipe(concatMap(q => concat(...q.map(this.p))),tap(c => console.log(c,'c')),toArray());
+    console.clear()
+    const example1 = this.rootSource$.pipe(concatMap(q => concat(...q.map(this.p))),
+                                  scan((rqr,crr) => [...rqr,  ...crr],[] ));
     example1.subscribe(console.log);
-    this.contacts = example1;
+    this.contacts = example1   
+
+
   }
 }
